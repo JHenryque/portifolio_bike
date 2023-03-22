c9c919f5a0378dd5c5ecb1e73e9ac50e2f6d365b
José Henryque <119264232+JHenryque@users.noreply.github.com>
2023-03-22 12:40:41 -0300

    projetos

---
+++ main.js
@@ -0,0 +1,9 @@
+let imagem = document.querySelectorAll(".wraper__banner")
+let bellts = document.querySelectorAll(".bellts")
+
+imagem.forEach(event => event.addEventListener('click', ()=> {
+}))
+
+
+
+setInterval(()=>  1000)
